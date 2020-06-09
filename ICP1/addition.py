d10296137b5e0fc1d149182102b6077cd21c3d96
vemula1802 <51023788+vemula1802@users.noreply.github.com>
2020-06-09 13:32:57 -0500

    Added screenshots

--- ICP1/addition.py
+++
@@ -1,7 +0,0 @@
-char1 = input("Enter the first number")
-char2 = input("Enter the second number")
-int1 = int(char1)
-int2 = int(char2)
-int3 = int1 + int2
-print("the sum of two numbers is : ", int3 )
-
