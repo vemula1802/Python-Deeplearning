d778f642d633e0c43ed1c2d1a09dbb669b3d2722
vemula1802 <51023788+vemula1802@users.noreply.github.com>
2020-06-09 13:17:56 -0500

    changes

---
+++ ICP1/Arithemetic.py
@@ -0,0 +1,13 @@
+char1 = input("Enter the first number : ")
+char2 = input("Enter the second number : ")
+int1 = int(char1)
+int2 = int(char2)
+int3 = int1 + int2
+int4 = int1 - int2
+int5 = int1 * int2
+int6 = int1 / int2
+print("the sum of two numbers is : ", int3 )
+print("the difference of two numbers is : ", int4 )
+print("the multiplication of two numbers is : ", int5 )
+print("the division of two numbers is : ", int6 )
+
