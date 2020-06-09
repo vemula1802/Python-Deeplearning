0c65ca4553e223724eaffad88b5432a637b6b8a5
vemula1802 <51023788+vemula1802@users.noreply.github.com>
2020-06-09 12:35:43 -0500

    Initial Commit

---
+++ ICP1/replace.py
@@ -0,0 +1,3 @@
+input1 = input("Please enter the sentence")
+print("The original sentence is : ",input1)
+print("The modified sentence is : ", input1.replace("python", "pythons"))
---
+++ ICP1/reverse.py
@@ -0,0 +1,4 @@
+char1 = input("Enter the string ")
+char2 = (char1[3:])
+print("The original input entered is : ", char1)
+print("The manipulated input is:",char2[::-1])
